6458e406c832af2892f823318b38841a45a0a8fd
Alfred Okedi <okedialf@gmail.com>
2022-04-09 23:02:59 +0300

    updated sleep time and limit lines to 5

--- list2md.go
+++ list2md.go
@@ -72,7 +72,7 @@ func main() {
 		if strings.HasPrefix(url, "https://github.com/") {
 			var repo Repo
 			var commit HeadCommit
-			time.Sleep(3 * time.Second)
+			time.Sleep(2 * time.Second)
             //repoAPI := fmt.Sprintf("https://api.github.com/repos/%s?access_token=%s", strings.TrimFunc(url[19:], trimSpaceAndSlash), accessToken)
 			repoAPI := fmt.Sprintf("https://api.github.com/repos/%s", strings.TrimFunc(url[19:], trimSpaceAndSlash))
 			fmt.Println(repoAPI)
